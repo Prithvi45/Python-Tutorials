2f47c362dc3c0f77961859e70629a2b7a95f0c2a
parmeetsinh <38753273+parmeetsinhvaghela@users.noreply.github.com>
2020-10-01 10:44:26 +0530

    Update python_decorators.py

--- python_decorators.py
+++ python_decorators.py
@@ -20,3 +20,27 @@ def sayHello(some):
 sayHello('Great')    #case 1
 
 sayHello('God')    #case 2
+
+
+
+# Decorators in Python 3
+# def function():
+#     print("hello word")
+#
+# func2 = function
+# del function
+# func2()
+
+def dec1(func):
+    def exec():
+        print("execute now")
+        func()
+        print("executed")
+    return exec
+
+@dec1          # this is decorator 
+def dec2():
+    print("pv is good")
+
+# pv = dec1(dec2)
+dec2()
